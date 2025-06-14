2bdeb9b992ce7dddea2f86eea0f3a005077c0b67
az-black <azmil.adhim20@gmail.com>
2025-06-14 19:39:30 +0700

    update say hello with parameter name

--- GoModule.go
+++ GoModule.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello()string{
-	return "Hello World Dunia Golang"
+func SayHello(name string)string{
+	return "Hello World Dunia Golang " + name
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/az-black/go-say-hello
+module github.com/az-black/go-say-hello/v2
 
 go 1.24.2
